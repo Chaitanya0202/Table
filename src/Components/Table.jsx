59421aef282c53832136b560f9e4134f40a83ff4
Chaitanya0202 <115619207+Chaitanya0202@users.noreply.github.com>
2024-04-29 09:07:00 +0530

    added features as filtering by Dept

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -38,35 +38,98 @@ function Table() {
       name: "Ghost",
       Department: "TL",
       Position: "optiona1",
-      Contact: 7878786,
+      Contact: 9996878786,
     },
     {
       id: 3,
       name: "yeet",
       Department: "HR",
       Position: "optiona1",
-      Contact: 7878786,
+      Contact: 8977878786,
     },
     {
       id: 4,
       name: "most",
       Department: "TL",
       Position: "optiona1",
-      Contact: 7878786,
+      Contact: 767878786,
     },
     {
       id: 5,
       name: "jeet",
       Department: "HR",
       Position: "optiona1",
-      Contact: 7878786,
+      Contact: 9077878786,
     },
     {
       id: 6,
       name: "host",
       Department: "TL",
       Position: "optiona1",
-      Contact: 7878786,
+      Contact: 9127878786,
+    },
+    {
+      id: 7,
+      name: "rock",
+      Department: "CEO",
+      Position: "optiona1",
+      Contact: 7127878786,
+    },
+    {
+      id: 8,
+      name: "tom",
+      Department: "manager",
+      Position: "optiona1",
+      Contact: 927878786,
+    },
+    {
+      id: 9,
+      name: "ford",
+      Department: "Support associate",
+      Position: "optiona1",
+      Contact: 997878786,
+    },
+    {
+      id: 10,
+      name: "carl",
+      Department: "engineer",
+      Position: "optiona1",
+      Contact: 9007878786,
+    },
+    {
+      id: 11,
+      name: "harry",
+      Department: "youtube",
+      Position: "youtuber",
+      Contact: 927878799,
+    },
+    {
+      id: 10,
+      name: "carl",
+      Department: "engineer",
+      Position: "optiona1",
+      Contact: 9007878786,
+    },
+    {
+      id: 11,
+      name: "harry",
+      Department: "youtube",
+      Position: "youtuber",
+      Contact: 927878799,
+    },
+    {
+      id: 10,
+      name: "carl",
+      Department: "engineer",
+      Position: "optiona1",
+      Contact: 9007878786,
+    },
+    {
+      id: 11,
+      name: "harry",
+      Department: "youtube",
+      Position: "youtuber",
+      Contact: 927878799,
     },
   ];
 
@@ -75,19 +138,30 @@ function Table() {
   const handleSearch = (event) => {
     const searchValue = event.target.value.toLowerCase();
     const filteredData = initialData.filter((row) =>
-      row.name.toLowerCase().includes(searchValue)
-    );
-    setData(filteredData);
-  };
+    row.name.toLowerCase().includes(searchValue)
+  );
+  setData(filteredData);
+};
+const handleSearchByDept=(event)=>{
+  const searchValue = event.target.value.toLowerCase();
+  const filteredData = initialData.filter((row) =>
+  row.Department.toLowerCase().includes(searchValue)
+);
+setData(filteredData);
+  
+  }
 
   return (
-    <div style={{ width: "70%", margin: "auto", marginTop: "15px" }}>
-      <div>
-        <input type="text" onChange={handleSearch} placeholder="Search by Name" />
+    <div style={{ width: "70%", margin: "auto", marginTop: "50px"}}>
+      <div >
+        <input type="text" onChange={handleSearch} placeholder="Search by Name"  />
+    
+        <input type="text" onChange={handleSearchByDept} placeholder="Search by Dept" style={{marginLeft:"20px"}} />
       </div>
       <DataTable
         columns={columns}
         data={data}
+        // expandableRows
         selectableRows
         fixedHeader
         pagination
